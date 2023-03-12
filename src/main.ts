e4ddb25d4f676d800fbef9322e1b3aa39d68ca22
Haltarys <45515869+Haltarys@users.noreply.github.com>
2023-03-12 16:46:53 +0100

    refactor(app): refactor app init

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-import * as http from 'http';
-import * as https from 'https';
-import { AppModule } from './app.module';
-import { LaunchConfigService } from './config/launch-config.service';
-import { SocketIoAdapter } from './utils/adapters/socket-io.adapter';
-import { MongoUniqueFieldFilter } from './utils/filters/mongo-unique-field.filter';
-import { CustomValidationPipe } from './utils/pipes/custom-validation.pipe';
+import { App } from './init';
 
 async function bootstrap() {
-  const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  app.useGlobalPipes(new CustomValidationPipe());
-  app.useGlobalFilters(new MongoUniqueFieldFilter());
+  const app = await App.init();
 
-  const launchConfigService = app.get(LaunchConfigService);
-
-  app.enableCors({ origin: launchConfigService.corsOrigin });
-
-  if (launchConfigService.isProduction) {
-    const httpsServer = https.createServer(
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      launchConfigService.httpsOptions!,
-      server,
-    );
-
-    const adapter = new SocketIoAdapter(
-      httpsServer,
-      launchConfigService.corsOrigin,
-    );
-    app.useWebSocketAdapter(adapter);
-    await app.init();
-    httpsServer.listen(launchConfigService.port);
-  } else {
-    const httpServer = http.createServer(server);
-    const adapter = new SocketIoAdapter(
-      httpServer,
-      launchConfigService.corsOrigin,
-    );
-    app.useWebSocketAdapter(adapter);
-    await app.init();
-    httpServer.listen(launchConfigService.port);
-  }
+  await app.start();
 }
 
 bootstrap();
