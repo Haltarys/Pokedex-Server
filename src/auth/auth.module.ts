4ce8d2202a13ff05a07528675945bc3fbeb22a43
Haltarys <45515869+Haltarys@users.noreply.github.com>
2023-03-26 17:16:55 +0200

    refactor(auth): use class to config jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Global, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtConfigService } from 'src/config/jwt-config.service';
 import { LaunchConfigService } from 'src/config/launch-config.service';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
@@ -14,17 +15,7 @@ import { LocalStrategy } from './strategies/local.strategy';
   imports: [
     UserModule,
     PassportModule,
-    JwtModule.registerAsync({
-      inject: [ConfigService, LaunchConfigService],
-      useFactory: (
-        configService: ConfigService,
-        launchConfigService: LaunchConfigService,
-      ) => ({
-        secret: configService.get<string>('jwt.secret'),
-        signOptions: { expiresIn: configService.get<string>('jwt.expiresIn') },
-        verifyOptions: { ignoreExpiration: !launchConfigService.isProduction },
-      }),
-    }),
+    JwtModule.registerAsync({ useClass: JwtConfigService }),
   ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy, LaunchConfigService],
---
+++ src/config/jwt-config.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import type { JwtModuleOptions, JwtOptionsFactory } from '@nestjs/jwt';
+import { LaunchConfigService } from './launch-config.service';
+
+@Injectable()
+export class JwtConfigService implements JwtOptionsFactory {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly launchConfigService: LaunchConfigService,
+  ) {}
+
+  createJwtOptions(): JwtModuleOptions {
+    return {
+      secret: this.configService.get<string>('jwt.secret'),
+      signOptions: {
+        expiresIn: this.configService.get<string>('jwt.expiresIn'),
+      },
+      verifyOptions: {
+        ignoreExpiration: !this.launchConfigService.isProduction,
+      },
+    };
+  }
+}
